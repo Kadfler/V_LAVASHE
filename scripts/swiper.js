bdb98717759c0c9c9f3fbecfc188ab4c57d943f9
KADFLER <mgav4751@Gmail.com>
2025-06-22 23:08:16 +0300

    адаптив освновных компонентов

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -3,7 +3,6 @@ new Swiper('.swiper-container')
 const swiper = new Swiper('.swiper', {
     loop: true,
             direction: "horizontal",
-            spaceBetween: 0,
 
             autoplay: {
 delay:2000
